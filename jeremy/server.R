5d3264c7ea711bc7ec3cc3344ab96f3c2969f87b
Jeremy Finke <jeremy.finke@gmail.com>
2015-01-03 09:25:04 -0600

    Mods
    Just changing some stuff around to see how the app works.

--- jeremy/server.R
+++ jeremy/server.R
@@ -2,10 +2,15 @@ shinyServer(function(input, output, session) {
   
   output$mychart <- renderLineChart({
     # Return a data frame. Each column will be a series in the line chart.
+#    data.frame(
+#      Sine = sin(1:100/10 + input$sinePhase * pi/180) * input$sineAmplitude,
+#      Cosine = 0.5 * cos(1:100/10),
+#      "Sine 2" = sin(1:100/10) * 0.25 + 0.5
+#    )
     data.frame(
-      Sine = sin(1:100/10 + input$sinePhase * pi/180) * input$sineAmplitude,
-      Cosine = 0.5 * cos(1:100/10),
+      Sine = c(1:100),
+      Cosine = c(100:1),
       "Sine 2" = sin(1:100/10) * 0.25 + 0.5
-    )
+      )
   })
 })
